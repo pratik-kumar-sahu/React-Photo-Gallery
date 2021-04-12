57e7a203901d7005dc210bc10ef491bc1bf7a742
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-12 17:21:15 +0530

    firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAqqR7hwf5oEofzwpos8Wx6GiwCZkC7vx4",
+  authDomain: "photo-gallery-firegram.firebaseapp.com",
+  projectId: "photo-gallery-firegram",
+  storageBucket: "photo-gallery-firegram.appspot.com",
+  messagingSenderId: "372070702307",
+  appId: "1:372070702307:web:4b23b3e5866b2efda539d6",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// invoking firestore and storage also
+const invokeFirestore = firebase.firestore();
+const invokeStorage = firebase.storage();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { invokeFirestore, invokeStorage, timestamp };
