04e645349b533a2f8993fa9e38ed50c59c90fbf9
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-23 23:38:45 +0530

    added login component to consume props from Auth

---
+++ src/comps/Login.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Login(props) {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailErr,
+    passErr,
+  } = props;
+
+  return (
+    <section className="login">
+      <div className="loginContainer">
+        <label>Username</label>
+        <input
+          type="text"
+          autoFocus
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className="errorMsg">{emailErr}</p>
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="errorMsg">{passErr}</p>
+        <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <button onClick={handleLogin}>Sign In</button>
+              <p>
+                Don't have an account ?{" "}
+                <span onClick={() => setHasAccount(!hasAccount)}>Sign Up</span>
+              </p>
+            </>
+          ) : (
+            <>
+              <button onClick={handleSignup}>Sign Up</button>
+              <p>
+                Already have an account ?{" "}
+                <span onClick={() => setHasAccount(!hasAccount)}>Sign In</span>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
